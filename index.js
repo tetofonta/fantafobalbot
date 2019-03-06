ade5a1cda8cbb7b615449c053929d26c36b34e49
Stefano <stefano.fontana.2000@gmail.com>
2019-03-06 13:15:47 +0100

    Setting up bot webhook pt 2.1.1

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const telegraf = require('telegraf');
-const fastify = require('fastify')();
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
 
-const bot = new telegraf(process.env.TELEGRAM_TOKEN);
-const port = process.env.PORT || 5000;
-
-bot.on('text', ({ reply }) => reply('Hello'));
-
-//fastify.use(bot.webhookCallback('/secret-path'));
-//bot.telegram.setWebhook('https://mysterious-island-36950.herokuapp.com/secret-path');
-
-fastify.get("/", (req, res) => {
-    res.send({ hello: 'world' })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
-fastify.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)
-});
+express()
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
