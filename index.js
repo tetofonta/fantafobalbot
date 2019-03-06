eaf9e04e573f2275ae1b50b42fb091b6ed2717d3
Stefano <stefano.fontana.2000@gmail.com>
2019-03-06 13:18:18 +0100

    Setting up bot webhook pt 2.2

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const { Pool } = require('pg');
+const {Pool} = require('pg');
+const telegraf = require("telegraf");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
+const bot = new telegraf(process.env.BOT_TOKEN);
+
+bot.telegram.setWebhook('https://mysterious-island-36950.herokuapp.com/secret-path');
+
 express()
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.send(results);
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use(bot.webhookCallback('/secret-path'))
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = {'results': (result) ? result.rows : null};
+            res.send(results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
