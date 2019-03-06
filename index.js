59cee621fe182073215ab2291ef20d9639cc4e03
Luca <luca@implast.it>
2019-03-06 13:41:27 +0100

    Change response 1.1.1.1.1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 
 const bot = new telegraf(process.env.TELEGRAM_TOKEN);
 
-bot.on('text', ({ reply }) => reply('Fottiti coglione, vieni tu al mio posto a lavorare 😒'));
+bot.on('text', ({ reply }) => reply('Ciaooooooo😄'));
 
 bot.telegram.setWebhook('https://mysterious-island-36950.herokuapp.com/secret-path');
 
