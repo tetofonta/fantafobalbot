44eddd90171974803f937fa81efc90e8ea3e9262
Stefano <stefano.fontana.2000@gmail.com>
2019-03-06 13:11:43 +0100

    Setting up bot webhook pt 2

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const telegraf = require('telegraf');
 const fastify = require('fastify')();
 
 const bot = new telegraf(process.env.TELEGRAM_TOKEN);
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 5000;
 
 bot.on('text', ({ reply }) => reply('Hello'));
 
-
 fastify.use(bot.webhookCallback('/secret-path'));
 bot.telegram.setWebhook('https://mysterious-island-36950.herokuapp.com/secret-path');
+
+fastify.get("/", (req, res) => {
+    res.send("HELLO")
+});
 
 fastify.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
